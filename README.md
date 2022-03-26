c46a50476e6053c006cfdbe06f12c749f6f906b1
conner-mcnicholas <connermcnicholas@gmail.com>
2022-03-25 19:35:29 -0700

    completed assignment

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # DataPipelineMiniProject
-From Section 15: Command Line and Data Pipelines
+
+run:
+```
+pip3 install -r requirements.txt
+initcsv.sh
+python pipeline.py
+```
+
+Program should output:
+```
+Creating table sales: OK
+Record inserted
+Record inserted
+Record inserted
+Record inserted
+Record inserted
+Record inserted
+Here are the top three events by number of tickets sold:
+    -Christmas Spectacular(5 tix sold)
+    -Washington Spirits vs Sky Blue FC(5 tix sold)
+    -The North American International Auto Show(4 tix sold)
+```
--- pipeline.py
+++ pipeline.py
@@ -1,5 +1,7 @@
 import mysql.connector
 from mysql.connector import errorcode
+from mysql.connector import Error
+import pandas as pd
 
 def get_db_connection():
     """
@@ -19,8 +21,8 @@ def initialize_database():
     cnx = get_db_connection()
     cursor = cnx.cursor()
     try:
-        cursor.execute(
-            "CREATE DATABASE {}".format(DB_NAME))
+        cursor.execute("DROP DATABASE {}".format(DB_NAME))
+        cursor.execute("CREATE DATABASE {}".format(DB_NAME))
     except mysql.connector.Error as err:
         print("Failed creating database: {}".format(err))
         exit(1)
@@ -40,10 +42,10 @@ def initialize_database():
     TABLES['sales'] = (
         "CREATE TABLE sales ("
         "  ticket_id INT NOT NULL,"
-        "  trans_date INT NOT NULL,"
+        "  trans_date DATE NOT NULL,"
         "  event_id INT NOT NULL,"
         "  event_name VARCHAR(50) NOT NULL,"
-        "  event_date date NOT NULL,"
+        "  event_date DATE NOT NULL,"
         "  event_type VARCHAR(10) NOT NULL,"
         "  event_city VARCHAR(20) NOT NULL,"
         "  customer_id INT NOT NULL,"
@@ -73,19 +75,20 @@ def load_third_party(connection, file_path_csv):
     schema of the table. You will need to use the Python connector to insert each record of the CSV
     file into the sales table.
     """
-    cursor = connection.cursor()
-    # [Iterate through the CSV file and execute insert statement]
-    sdata = pd.read_csv(file_path_csv, index_col=False, delimiter = ',')
-    #loop through the data frame
-    for i,row in sdata.iterrows():
-        #here %S means string values
-        sql = "INSERT INTO pipeline.sales VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
-        cursor.execute(sql, tuple(row))
-        print("Record inserted")
-        # the connection is not auto committed by default, so we must commit to save our changes
-        conn.commit()
-        except Error as e:
-            print("Error while connecting to MySQL", e)
+    try:
+        cursor = connection.cursor()
+        # [Iterate through the CSV file and execute insert statement]
+        sdata = pd.read_csv(file_path_csv, index_col=False, delimiter = ',')
+        #loop through the data frame
+        for i,row in sdata.iterrows():
+            #here %S means string values
+            sql = "INSERT INTO pipeline.sales VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+            cursor.execute(sql, tuple(row))
+            print("Record inserted")
+            # the connection is not auto committed by default, so we must commit to save our changes
+            connection.commit()
+    except Error as e:
+        print("Error while connecting to MySQL", e)
 
     connection.commit()
     cursor.close()
@@ -99,7 +102,7 @@ def query_popular_tickets(connection):
     past month.
     """
     # Get the most popular ticket in the past month
-    sql_statement = "<your sql statement>"
+    sql_statement = "WITH top_three AS (SELECT event_name,sum(num_tickets) AS numsold FROM sales group by 1 order by 2 desc,1 asc LIMIT 3) SELECT concat(event_name,'(',numsold) AS top_events from top_three;"
     cursor = connection.cursor()
     cursor.execute(sql_statement)
     records = cursor.fetchall()
@@ -115,8 +118,13 @@ def display_the_result(res):
         - Carlisle Ford Nationals
         - Washington Spirits vs Sky Blue FC
     """
+    print('Here are the top three events by number of tickets sold:')
+    for i in res:
+        print('    -'+i[0]+' tix sold)')
 
 if __name__ == "__main__":
     DB_NAME = 'pipeline'
     cnx = initialize_database()
     load_third_party(cnx,'/home/conner/DataPipelineMiniProject/third_party_sales_full.csv')
+    result = query_popular_tickets(cnx)
+    display_the_result(result)
